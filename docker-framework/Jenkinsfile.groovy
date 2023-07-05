d691013af51b9253f9ab43c8aae5d1d640f5e1b1
slash-l <slash_mumu@163.com>
2023-07-05 11:02:38 +0800

    注释掉部分 steps

--- docker-framework/Jenkinsfile.groovy
+++ docker-framework/Jenkinsfile.groovy
@@ -59,7 +59,6 @@ node {
                 println "Docker pushing -->" + tagName + " To " + FRAMEWORK_REPO
                 // buildInfo = rtDocker.push(tagName, FRAMEWORK_REPO, buildInfo)
                 buildInfo = rtDocker.push tagName, FRAMEWORK_REPO
-                println("rtDocker:" + buildInfo)
 
                 println "Docker Buildinfo"
                 rtServer.publishBuildInfo buildInfo
@@ -133,55 +132,55 @@ node {
     // }
 }
 
-def updateDockerFile () {
-    def BUILD_NUMBER = env.BUILD_NUMBER
-    // sh "sed -i 's/docker.artifactory/${ARTDOCKER_REGISTRY}/' Dockerfile"
-    sh 'sed -i "s/docker-framework:latest/docker-framework:$BUILD_NUMBER/" Dockerfile'
-}
+// def updateDockerFile () {
+//     def BUILD_NUMBER = env.BUILD_NUMBER
+//     // sh "sed -i 's/docker.artifactory/${ARTDOCKER_REGISTRY}/' Dockerfile"
+//     sh 'sed -i "s/docker-framework:latest/docker-framework:$BUILD_NUMBER/" Dockerfile'
+// }
 
-def reTagLatest (targetRepo) {
-     def BUILD_NUMBER = env.BUILD_NUMBER
-     sh 'sed -E "s/@/$BUILD_NUMBER/" retag.json > retag_out.json'
-     switch (targetRepo) {
-          case PROMOTE_REPO :
-              sh 'sed -E "s/TARGETREPO/${PROMOTE_REPO}/" retag_out.json > retaga_out.json'
-              break
-          case SOURCE_REPO :
-               sh 'sed -E "s/TARGETREPO/${SOURCE_REPO}/" retag_out.json > retaga_out.json'
-               break
-      }
-      sh 'cat retaga_out.json'
-      withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: CREDENTIALS, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-          def curlString = "curl -u " + env.USERNAME + ":" + env.PASSWORD + " " + SERVER_URL
-          def regTagStr = curlString +  "/api/docker/${SOURCE_REPO}/v2/promote -X POST -H 'Content-Type: application/json' -T retaga_out.json"
-          println "Curl String is " + regTagStr
-          sh regTagStr
-      }
-}
+// def reTagLatest (targetRepo) {
+//      def BUILD_NUMBER = env.BUILD_NUMBER
+//      sh 'sed -E "s/@/$BUILD_NUMBER/" retag.json > retag_out.json'
+//      switch (targetRepo) {
+//           case PROMOTE_REPO :
+//               sh 'sed -E "s/TARGETREPO/${PROMOTE_REPO}/" retag_out.json > retaga_out.json'
+//               break
+//           case SOURCE_REPO :
+//                sh 'sed -E "s/TARGETREPO/${SOURCE_REPO}/" retag_out.json > retaga_out.json'
+//                break
+//       }
+//       sh 'cat retaga_out.json'
+//       withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: CREDENTIALS, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+//           def curlString = "curl -u " + env.USERNAME + ":" + env.PASSWORD + " " + SERVER_URL
+//           def regTagStr = curlString +  "/api/docker/${SOURCE_REPO}/v2/promote -X POST -H 'Content-Type: application/json' -T retaga_out.json"
+//           println "Curl String is " + regTagStr
+//           sh regTagStr
+//       }
+// }
 
-// Following test will work only if you have jenkins running outside kubernetes.
-//test docker image by runnning container
-def testFramework (tag) {
-    def result = true
-    docker.image(tag).withRun('-p 8181:8181') {c ->
-        sleep 10
-        //def stdout = sh(script: 'curl "http://localhost:8181/swampup/"', returnStdout: true)
-        //if (stdout.contains("Welcome Docker Lifecycle Training")) {
-            // println "*** Passed Test: " + stdout
-        //} else {
-         //   println "*** Failed Test: " + stdout
-        //    result = false
-        //}
-    }
-    sh "docker rmi ${tag}"
-    return true
-}
+// // Following test will work only if you have jenkins running outside kubernetes.
+// //test docker image by runnning container
+// def testFramework (tag) {
+//     def result = true
+//     docker.image(tag).withRun('-p 8181:8181') {c ->
+//         sleep 10
+//         //def stdout = sh(script: 'curl "http://localhost:8181/swampup/"', returnStdout: true)
+//         //if (stdout.contains("Welcome Docker Lifecycle Training")) {
+//             // println "*** Passed Test: " + stdout
+//         //} else {
+//          //   println "*** Failed Test: " + stdout
+//         //    result = false
+//         //}
+//     }
+//     sh "docker rmi ${tag}"
+//     return true
+// }
 
-def updateProperty (property) {
-    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: CREDENTIALS, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            def curlString = "curl -u " + env.USERNAME + ":" + env.PASSWORD + " " + "-X PUT " + SERVER_URL
-            def updatePropStr = curlString +  "/api/storage/${SOURCE_REPO}/docker-framework/${env.BUILD_NUMBER}?properties=${property}"
-            println "Curl String is " + updatePropStr
-            sh updatePropStr
-     }
-}
+// def updateProperty (property) {
+//     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: CREDENTIALS, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+//             def curlString = "curl -u " + env.USERNAME + ":" + env.PASSWORD + " " + "-X PUT " + SERVER_URL
+//             def updatePropStr = curlString +  "/api/storage/${SOURCE_REPO}/docker-framework/${env.BUILD_NUMBER}?properties=${property}"
+//             println "Curl String is " + updatePropStr
+//             sh updatePropStr
+//      }
+// }
